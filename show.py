fa9277732041104afe4d72333f553d125e2884aa
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-12-07 14:51:50 +0900

    オフトゥン

--- show.py
+++ show.py
@@ -20,24 +20,24 @@ with open('record.json') as f:
 for generation in list(data.keys())[:8]:  # 0から7までのgenerationのみを処理
     for individual in range(len(data[generation])):
         child = int(individual)
-        child_coords = (child ,int(generation))
+        child_coords = (child / 64, int(generation) / 8)  # 座標を0から1の範囲に変換
 
         # par1が存在する場合のみ矢印を描画
         if 'par1' in data[str(generation)][str(individual)]:
             parent1 = int(data[generation][str(individual)]['par1'])
-            parent1_coords = (parent1 , int(generation) - 1)  # parent1は前の世代
-            axs[parent1_coords].annotate("", xy=child_coords, xytext=parent1_coords, 
-                                         xycoords='data', 
+            parent1_coords = (parent1 / 64, (int(generation) - 1) / 8)  # 座標を0から1の範囲に変換
+            axs[parent1_coords].annotate("", xy=child_coords, xytext=parent_coords, 
+                                         xycoords='figure fraction', 
+                                         textcoords='figure fraction',
                                          arrowprops=dict(arrowstyle="->", color='r'))
 
         # par2が存在する場合のみ矢印を描画
         if 'par2' in data[str(generation)][str(individual)]:
             parent2 = int(data[generation][str(individual)]['par2'])
-            parent2_coords = (parent2 ,int(generation) - 1)  # parent2は前の世代
+            parent2_coords = (parent2 / 64, (int(generation) - 1) / 8)  # 座標を0から1の範囲に変換
             axs[parent2_coords].annotate("", xy=child_coords, xytext=parent2_coords, 
-                                         xycoords='data', 
+                                         xycoords='figure fraction', 
+                                         textcoords='figure fraction',
                                          arrowprops=dict(arrowstyle="->", color='r'))
-
-# 画像として保存
 plt.subplots_adjust(wspace=0.5)  # 画像間のスペースを調整
 plt.savefig("grid.png")
